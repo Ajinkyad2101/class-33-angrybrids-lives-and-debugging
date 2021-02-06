02d03f37754da03fae54f0387b3c6477a6a3c5e8
Ajinkyad2101 <74656150+Ajinkyad2101@users.noreply.github.com>
2021-02-06 19:00:32 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,27 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimg= loadImage("sprites/smoke.png");
+    this.path=[]
+  }
+
+  display() {     
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if(gameState==="launch"){
+    if(this.body.position.x>250 && this.body.speed>10){
+  var  position=[this.body.position.x,this.body.position.y]
+  this.path.push(position)
+    }
+
+  for(var i=0  ;  i<this.path.length  ;  i++){
+   image(this.smokeimg,this.path[i][0],this.path[i][1])
+   console.log(this.path[i])
+   //console.log(this.path.length)
+  }
+}
+    super.display();
+  }
+
+}
